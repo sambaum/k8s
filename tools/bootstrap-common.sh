8a7252707a1ef0ee04804769e630b08cde88b29b
Samuel Baumgartner <samuel.baumgartner@gmail.com>
2025-09-04 22:39:43 +0200

    no brew

--- tools/bootstrap-common.sh
+++ tools/bootstrap-common.sh
@@ -11,26 +11,26 @@ then
     exit 1
 fi
 
-# Check if Homebrew is installed
-if ! command -v brew &> /dev/null
-then
-    echo "Homebrew is not installed. Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo
-    echo -e "${RED}Homebrew should now be installed. Open a new shell and run the script again ${NOCOLOR}"
-    exit 1
-else
-    echo "Homebrew is already installed."
-fi
+# # Check if Homebrew is installed
+# if ! command -v brew &> /dev/null
+# then
+#     echo "Homebrew is not installed. Installing Homebrew..."
+#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#     echo
+#     echo -e "${RED}Homebrew should now be installed. Open a new shell and run the script again ${NOCOLOR}"
+#     exit 1
+# else
+#     echo "Homebrew is already installed."
+# fi
 
-if ! dpkg -s build-essential >/dev/null 2>&1; then
-  echo build-essential not yet installed. Installing...
-  sudo apt-get update
-  sudo apt-get install -y build-essential
-fi
+# if ! dpkg -s build-essential >/dev/null 2>&1; then
+#   echo build-essential not yet installed. Installing...
+#   sudo apt-get update
+#   sudo apt-get install -y build-essential
+# fi
 
-brew install gcc 
-brew install derailed/k9s/k9s fluxcd/tap/flux sops age
+# brew install gcc 
+# brew install derailed/k9s/k9s fluxcd/tap/flux sops age
 
 # Check if GITHUB_TOKEN is set
 if [ -z "${GITHUB_TOKEN}" ]; then
