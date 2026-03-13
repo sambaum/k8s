8866ddfe7792b6e9d07194f5fd5c5bc0df00522a
Samuel Baumgartner <samuel.baumgartner@gmail.com>
2026-03-13 11:34:57 +0100

    improve bootstrap scripts

--- clusters/deskcube/bootstrap.sh
+++ clusters/deskcube/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-. ../../tools/bootstrap-common.sh
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+. "$SCRIPT_DIR/../../tools/bootstrap-common.sh"
 
 GITHUB_USER=sambaum
 REPOSITORY=k8s
