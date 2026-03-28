1118255044ff4d0d8960ad822059e1fb56c2157e
Samuel Baumgartner <samuel.baumgartner@gmail.com>
2026-03-28 01:37:18 +0100

    less logging

--- tools/validate.sh
+++ tools/validate.sh
@@ -66,12 +66,13 @@ echo "INFO - Downloading Flux OpenAPI schemas"
 mkdir -p /tmp/flux-crd-schemas/master-standalone-strict
 curl -sL https://github.com/fluxcd/flux2/releases/latest/download/crd-schemas.tar.gz | tar zxf - -C /tmp/flux-crd-schemas/master-standalone-strict
 
+echo "INFO - Validating YAML syntax..."
 find . "${EXCLUDE_ARGS[@]}" -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
-	echo "INFO - Validating $file"
+	# echo "INFO - Validating $file"
 	yq e 'true' "$file" >/dev/null
 done
 
-kubeconform_config=("-strict" "-ignore-missing-schemas" "-schema-location" "default" "-schema-location" "/tmp/flux-crd-schemas" "-verbose" "-skip=Secret")
+kubeconform_config=("-strict" "-ignore-missing-schemas" "-schema-location" "default" "-schema-location" "/tmp/flux-crd-schemas" "-skip=Secret") # "-verbose"
 
 echo "INFO - Validating clusters"
 find ./clusters -maxdepth 2 "${EXCLUDE_ARGS[@]}" -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
@@ -87,7 +88,7 @@ kustomize_config="kustomization.yaml"
 
 echo "INFO - Validating kustomize overlays"
 find . "${EXCLUDE_ARGS[@]}" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
-	echo "INFO - Validating kustomization ${file/%$kustomize_config/}"
+	# echo "INFO - Validating kustomization ${file/%$kustomize_config/}"
 	kustomize build "${file/%$kustomize_config/}" "${kustomize_flags[@]}" |
 		kubeconform "${kubeconform_config[@]}"
 	if [[ ${PIPESTATUS[0]} != 0 ]]; then
