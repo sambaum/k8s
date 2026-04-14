7b88e5c701b528ec8a2997843aeb6e3a31505b7e
Samuel Baumgartner <samuel.baumgartner@gmail.com>
2026-04-14 11:07:17 +0200

    show filename

--- tools/validate.sh
+++ tools/validate.sh
@@ -89,7 +89,8 @@ kustomize_config="kustomization.yaml"
 echo "INFO - Validating kustomize overlays"
 find . "${EXCLUDE_ARGS[@]}" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
 	# echo "INFO - Validating kustomization ${file/%$kustomize_config/}"
-	kustomize build "${file/%$kustomize_config/}" "${kustomize_flags[@]}" |
+	kustomize build "${file/%$kustomize_config/}" "${kustomize_flags[@]}" \
+		2> >(sed "s|^|  [${file}]: |" >&2) |
 		kubeconform "${kubeconform_config[@]}"
 	if [[ ${PIPESTATUS[0]} != 0 ]]; then
 		exit 1
