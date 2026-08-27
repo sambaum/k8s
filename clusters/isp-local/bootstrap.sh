6d5e495c56f6a722742e52e1765f2f26b84bdca2
Samuel Baumgartner <samuel.baumgartner@gmail.com>
2026-08-27 14:00:52 +0200

    add isp-local

---
+++ clusters/isp-local/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+. "$SCRIPT_DIR/../../tools/bootstrap-common.sh"
+
+GITHUB_USER=sambaum
+REPOSITORY=k8s
+BRANCH=master
+ENVIRONMENT=isp-local
+
+flux bootstrap github \
+	--owner=$GITHUB_USER \
+	--repository=$REPOSITORY \
+	--branch=$BRANCH \
+	--path=./clusters/$ENVIRONMENT \
+	--personal
